154bc28dd10a0318ccb26818ff2a1ded538a83b4
dazariani <davitazariani@gmail.com>
2024-09-03 23:29:49 +0400

    Create project setup

---
+++ delivery_app/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+  admin = models.BooleanField(default=False)
+  customer = models.BooleanField(default=False)
+  courier = models.BooleanField(default=False)
+
+  
+
+class Parcel(models.Model):
+  PARCEL_STATUS_CHOICES = [
+  ("Pending", "Pending"),
+  ("In Transit", "In Transit"),
+  ("Delivered", "Delivered"),
+  ]
+  
+  title = models.CharField(max_length=50)
+  description = models.TextField(max_length=200)
+  status = models.CharField(choices=PARCEL_STATUS_CHOICES)
+  sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE) 
+  receiver_name = models.CharField(max_length=50)
+  receiver_address = models.TextField(max_length=150)
+  courier = models.ForeignKey(CustomUser, related_name='courier_parcels', null=True, on_delete=models.CASCADE) 
+  created_at = models.DateTimeField(auto_now_add=True)
+  delivered_at = models.DateTimeField(null=True)
+
+  def __str__(self):
+    return self.title
+
+
+class DeliveryProof(models.Model):
+  parcel = models.OneToOneField(Parcel, on_delete=models.CASCADE) 
+  image = models.ImageField(upload_to='images/')
+  timestamp = models.DateTimeField(auto_now_add=True) 
+
+  def __str__(self):
+    return self.parcel
---
+++ delivery_app/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import CustomUser, Parcel, DeliveryProof
+
+class CustomUserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = CustomUser
+    fields = ['id', 'username', 'password', 'admin', 'customer', 'courier']
+    extra_kwargs = {
+      'password': {'write_only': True}
+    }
+
+  def create(self, validated_data):
+    password = validated_data.pop('password', None)
+    instance = self.Meta.model(**validated_data)
+    if password is not None:
+      instance.set_password(password)
+    instance.save()
+    return instance
+  
+
+class ParcelSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Parcel
+    fields = '__all__'  
+
+
+class DeliveryProofSerializer(serializers.ModelField):
+  class Meta:
+    model = DeliveryProof
+    fields = '__all__'
